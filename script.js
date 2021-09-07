e71458184e545f67bd5537159b629c72821317ee
Antonio Saldivar <jediknight_01@hotmail.com>
2021-09-06 19:25:36 -0500

    Added object to create messages

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Hi there!');
+const messages = {
+  memoriesFragments: ['when you said "Increibla"?', 'when we first danced?', 'our first kiss?'],
+  loveFragments: ['how smart you are', 'your smile', 'the way you walk'],
+  datesFragments: ['go snowboarding', 'take a dancing class', 'go to a the beach'],
+  randomSelection(a) {
+    return Math.floor(Math.random()* a.length);
+  },
+  createMemory() {
+    const randomIndex = this.randomSelection(this.memoriesFragments);
+    const data = this.memoriesFragments[randomIndex];
+    console.log(`Do you remember ${data}`);
+  },
+  createIlu() {
+    const randomIndex = this.randomSelection(this.loveFragments);
+    const data = this.loveFragments[randomIndex];
+    console.log(`I love ${data}`);
+  },
+  createDate() {
+    const randomIndex = this.randomSelection(this.datesFragments);
+    const data = this.datesFragments[randomIndex];
+    console.log(`We should ${data}`);
+  },
+  callElements() {
+    this.createMemory(),
+    this.createIlu(),
+    this.createDate()
+  }
+}
+
+messages.callElements();
